15809c91af19ddb2b6e01a2114c0604147bf0fb6
Judith Risse <jrisse@headnode1.genepool.private>
2016-01-13 13:43:18 +0000

    initial commit

---
+++ parse_slocs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import struct
+import math
+
+
+fn = 'X_s.locs'
+f= open(fn, 'rb')
+o = open('X_s.locs.txt','w')
+try:
+    buf = f.read(12)
+    o.write("##%s\n"%(str(struct.unpack('=ifI', buf))))
+    buf = f.read(8)
+    while len(buf) == 8:
+        t = struct.unpack('=ff', buf)
+        x = int(t[0] * 10.0 + 1000.5)
+        y = int(t[1] * 10.0 + 1000.5)
+        o.write("%s\t%s\n"%(x,y))
+        buf = f.read(8)
+finally:
+    o.close()
+    f.close()
