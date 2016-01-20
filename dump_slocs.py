5c0468d90addb0d76143fbdf935a879928c59347
Tim Booth <tim.booth@ed.ac.uk>
2016-01-20 14:48:59 +0000

    Refactored so Judith can use the parser programattically.

--- dump_slocs.py
+++ dump_slocs.py
@@ -12,22 +12,41 @@ import sys
 from signal import signal, SIGPIPE, SIG_DFL
 signal(SIGPIPE,SIG_DFL)
 
-f = None
-try:
-    f = open(sys.argv[1], 'rb')
-except IndexError:
-    f = sys.stdin
+def main():
+    f = None
+    try:
+        f = open(sys.argv[1], 'rb')
+    except IndexError:
+        f = sys.stdin
 
-o = sys.stdout
+    o = sys.stdout
 
-try:
-    buf = f.read(12)
+    try:
+        header = yield_header(f)
+        o.write("##%s\n"%(str(header)))
+        clusternumpad = len(str(header[2]))
+
+        for coords in enumerate(yield_coords(f, header)):
+            #Write out "%i %i:%i\n"% (clusternum,x,y)
+            #But I made it ugly by also interpolating the padding size
+            o.write(("%%0%ii %%i:%%i\n"%clusternumpad) % (coords[:1] + coords[1][:]))
+            #o.write(str(coords) + "\n")
+
+    finally:
+        o.close()
+        f.close()
+
+def yield_header(f):
     #First 3*4 bytes are the header
-    header = struct.unpack('<ifI', buf)
-    o.write("##%s\n"%(str(header)))
+    buf = f.read(12)
+    return struct.unpack('<ifI', buf)
+
+def yield_coords(f, header):
+    #You must have read the header first
+    assert f.tell() >= 12
+
     buf = f.read(8)
     clusternum = 0
-    clusternumpad = len(str(header[2]))
     while len(buf) == 8:
         # Each following 8 bytes are a co-ordinate pair as detailed in
         # https://broadinstitute.github.io/picard/javadoc/picard/picard/illumina/parser/readers/LocsFileReader.html
@@ -36,11 +55,9 @@ try:
         t = struct.unpack('<ff', buf)
         x = int(t[0] * 10.0 + 1000.5)
         y = int(t[1] * 10.0 + 1000.5)
-        #Write out "%i %i:%i\n"% (clusternum,x,y)
-        #But I made it ugly by also interpolating the padding size
-        o.write(("%%0%ii %%i:%%i\n"%clusternumpad) % (clusternum,x,y))
+
+        yield (x,y)
         clusternum += 1
         buf = f.read(8)
-finally:
-    o.close()
-    f.close()
+
+main()
