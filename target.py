46b27e138e4b4998094a6bcc92ebd481e95f40f6
Tim Booth <tim.booth@ed.ac.uk>
2016-01-25 15:07:46 +0000

    Target container done and tested

--- target.py
+++ target.py
@@ -1,32 +1,111 @@
 #!python
+from __future__ import print_function, division, absolute_import
 
-def load_targets(filename):
+def load_targets(filename, levels):
 
-    return AllTargets
+    all_targets = AllTargets()
+
+    with open(filename, 'r') as coord_fh:
+
+        #There are various smart-arse ways to read a file N lines
+        #at a time, but I'll just do it the obvious dumb way.
+        while True:
+
+            targ_lines = []
+            for l in range(levels):
+                aline = coord_fh.readline().rstrip()
+
+                if aline == '' and len(targ_lines) == 0:
+                    #EOF - we're done
+                    return all_targets
+                elif aline == '':
+                    raise Exception("Found blank line or EOF in the middle of a record")
+
+                targ_lines.append(aline)
+
+            all_targets.add_target([ map(int,x.split(',')) for x in targ_lines])
+
+    #This function returns an all_targets object, but it only exits in the middle!
 
 
 class AllTargets:
 
+    def __init__(self):
+        self._target_dict = dict()
+
+        # This will contain [ index : [ target, target, ... ] ]
+        self._reverse_lookup = dict()
+
     def get_all_targets(self):
 
-        return [Target]
+        return self._target_dict.values()
 
-    def get_all_indices(level = None):
+    def get_target_by_centre(self, centre):
+
+        return self._target_dict[centre]
+
+    def add_target(self, coords):
+
+        new_target = Target(coords)
+
+        #You shouldn't add the same target twice
+        assert new_target.get_centre() not in self._target_dict
+
+        self._target_dict[new_target.get_centre()] = new_target
+
+        #As well as indexing by centre, hash all indices
+        for idx in new_target.get_indices():
+            if idx in self._reverse_lookup:
+                self._reverse_lookup[idx].append(new_target)
+            else:
+                self._reverse_lookup[idx] = [new_target]
+
+
+    def get_all_indices(self, level = None):
+
+        if level == 0:
+            #Do it the quick way
+            return list(self._target_dict.keys())
+        if level is None:
+            #Flatten the list (standard Python-ism)
+            return [ y for x in self.get_all_targets() for y in x.get_indices(level) ]
 
     def get_from_index(self, index):
 
-        return [(Target, int)]
+        res = []
+        for target in self._reverse_lookup[index]:
+            res.append( (target, target.get_level_from_index(index)) )
+
+        return res
 
 class Target:
-    def __init__(self, data):
+    def __init__(self, coords):
         """Parse the data which is in the format of ...
         """
+        assert len(coords[0]) == 1
+        #centre = coords[0][0]
 
-    def get_indices(level = None):
+        self.coords = coords
 
-        return []
+    def get_indices(self, level = None):
 
+        if level is None:
+            #Flatten the list (standard Python-ism)
+            return [ y for x in self.coords for y in x ]
+        else:
+            return self.coords[level]
+
+    def get_centre(self):
+
+        return self.get_indices(0)[0]
 
     def get_level_from_index(self, index):
 
-        return int
+        for lev, arr in enumerate(self.coords):
+            if index in arr:
+                return lev
+
+        #Do we want this??
+        #raise Exception("No such index")
+        # or this?
+        return None
---
+++ test/test_target.py
@@ -0,0 +1,78 @@
+#!python
+from __future__ import print_function, division, absolute_import
+
+import sys
+import unittest
+import time
+
+try:
+    from target import load_targets
+except:
+    #If this fails, you is probably running the tests wrongly
+    print("****",
+          "You want to run these tests by using:",
+          "  python -m unittest test.test_target",
+          "or even",
+          "  python -m unittest discover",
+          "****",
+          sep="\n")
+    raise
+
+# These tests use a subset of the file hiseq_4000_10000clusters.list
+
+TEST_FILE = 'test/small.list'
+
+class TestTargetReader(unittest.TestCase):
+
+    all_targets = None
+
+    #Load the targets each time as the file is so small
+    def setUp(self):
+        self.all_targets = load_targets(TEST_FILE, 4)
+
+    def test_num_targets(self):
+        all_targets = self.all_targets
+
+        #There should be 7 of them
+        self.assertEquals(len(all_targets.get_all_targets()), 7)
+
+        #Ditto if we do it this way
+        self.assertEquals(len(set(all_targets.get_all_indices(0))), 7)
+
+    def test_lookups(self):
+        all_targets = self.all_targets
+
+        #If I look up point 196654 it should be the centre of cluster 6
+        res = all_targets.get_from_index(196654)
+
+        target_for_196654 = res[0][0]
+        self.assertEqual(res, [ ( target_for_196654, 0 ) ])
+
+        self.assertEqual(target_for_196654.get_centre(), 196654)
+
+        res2 = target_for_196654.get_indices(1)
+        self.assertEqual(res2, map(int,'195083,195084,196653,196655,198225,198226'.split(',')))
+
+    def test_multiple_appearances(self):
+        all_targets = self.all_targets
+
+        #The file should contain exactly 213 distinct spots, as revealed by
+        #sed 's/,/\n/g' test/small.list | sort -u | wc
+        self.assertEqual(len(set(all_targets.get_all_indices())), 213)
+
+        #Point 1030466 should pop up twice in rank 2 and once in rank 3
+        res = all_targets.get_from_index(1030466)
+
+        self.assertEqual(len(res), 3)
+
+        self.assertEqual(sorted([ x[1] for x in res ]), [2,2,3])
+
+    def test_bad_add(self):
+        all_targets = self.all_targets
+
+        self.assertRaises(Exception, all_targets.add_target, [(1, 2), (3, 4)])
+
+        #This should work, but only once
+        all_targets.add_target( [(111,), (112, 113, 114, 115)] )
+        self.assertRaises(Exception, all_targets.add_target, [(111,), (112, 113, 114, 115)])
+
