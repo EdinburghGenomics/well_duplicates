85fffd1b806adc864ef59e5dc4f726851c44fe60
Tim Booth <tim.booth@ed.ac.uk>
2016-01-25 10:30:54 +0000

    Add outline for a Target manager class, to look up locations for circles of wells

---
+++ target.py
@@ -0,0 +1,32 @@
+#!python
+
+def load_targets(filename):
+
+    return AllTargets
+
+
+class AllTargets:
+
+    def get_all_targets(self):
+
+        return [Target]
+
+    def get_all_indices(level = None):
+
+    def get_from_index(self, index):
+
+        return [(Target, int)]
+
+class Target:
+    def __init__(self, data):
+        """Parse the data which is in the format of ...
+        """
+
+    def get_indices(level = None):
+
+        return []
+
+
+    def get_level_from_index(self, index):
+
+        return int
